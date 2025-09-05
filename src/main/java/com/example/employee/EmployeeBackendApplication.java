44d1a364b3c69124fc88e90efc237ebe8a311b61
ruchithavemula <2300032021@kluniversity.in>
2025-09-05 12:10:50 +0530

    updated

--- src/main/java/com/example/employee/EmployeeBackendApplication.java
+++ src/main/java/com/example/employee/EmployeeBackendApplication.java
@@ -2,12 +2,18 @@ package com.example.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeBackendApplication {
+public class EmployeeBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeBackendApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeBackendApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBackendApplication.class, args);
+    }
 }
